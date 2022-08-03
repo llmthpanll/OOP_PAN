2577edc9df41221e18d690233b3f4ba38b11bee0
Methasit <54169574+llmthpanll@users.noreply.github.com>
2022-08-03 10:28:08 +0700

    week1

---
+++ CircleArea.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius, area;
+        Scanner input = new Scanner(System.in);
+        radius = input.nextDouble();
+        area = Math.PI * radius * radius;
+        System.out.println(area);
+    }
+}
