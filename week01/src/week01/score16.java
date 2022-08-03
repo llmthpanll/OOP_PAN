7beb536496247331433e30bf3a26994b93339fda
llmthpanll <methasitpan@gmail.com>
2022-08-03 22:41:15 +0700

    16

---
+++ week01/src/week01/score16.java
@@ -0,0 +1,16 @@
+package week01;
+import java.util.*;
+public class score16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner ad = new Scanner(System.in);
+        Scanner fe = new Scanner(System.in);
+        int x, y, z;
+        double avg;
+        x = sc.nextInt();
+        y = ad.nextInt();
+        z = fe.nextInt();
+        avg = (x + y + z)/3;
+        System.out.println(avg);
+    }
+}
