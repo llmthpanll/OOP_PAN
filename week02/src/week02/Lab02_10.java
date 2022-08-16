04944b7e8d3e722ac5f4eb33c837c044d1f12a44
llmthpanll <methasitpan@gmail.com>
2022-08-16 22:33:24 +0700

    week02

---
+++ week02/src/week02/Lab02_10.java
@@ -0,0 +1,36 @@
+package week02;
+import java.util.*;
+public class Lab02_10 {
+    public static void main(String[] args) {
+        Scanner inputString = new Scanner(System.in);
+        System.out.print("Please insert your name : ");
+        String name = inputString.nextLine();
+        Scanner inputInt = new Scanner(System.in);
+        System.out.print("Please insert your age : ");
+        int age = inputInt.nextInt();
+        System.out.print("Please insert number of working days : ");
+        int numDay1 = inputInt.nextInt();
+        System.out.print("Please insert number of absent days : ");
+        int numDay2 = inputInt.nextInt();
+        System.out.print("Please insert your body weight : ");
+        int weight = inputInt.nextInt();
+        System.out.println("Hi, " + name);
+        int salary;
+        if(age >= 21 && age <= 30){
+            salary = (numDay1 * 300) - (numDay2 * 50);
+        } else if(age >= 31 && age <= 40){
+            salary = (numDay1 * 500) - (numDay2 * 50);
+        } else if(age >= 41 && age <= 50){
+            salary = (numDay1 * 1000) - (numDay2 * 25);
+        } else{
+            salary = (numDay1 * 3000);
+        }
+        System.out.println("Your salary is "+salary+" Baht");
+        if(weight >= 10 && weight <= 60){
+            salary = salary + 5000;
+        } else if(weight >= 61 && weight <= 90){
+            salary = salary + (5000 - ((weight - 60) * 10));
+        }
+        System.out.println("Your salary and bonus is "+salary+" Baht");
+    }
+}
---
+++ week02/src/week02/banking.java
@@ -0,0 +1,25 @@
+package week02;
+import java.util.*;
+public class banking {
+    public static void main(String[] args) {
+        double money;
+        char bank;
+        double total;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input your money : ");
+        money = sc.nextDouble();
+        System.out.print("Input your account type (Please type A B C or X in uppercase) : ");
+        bank = sc.next().charAt(0);
+        switch (bank) {
+            case 'A': total = money * 1.015;
+                    break;
+            case 'B': total = money * 1.02;
+                    break;
+            case 'C': total = money * 1.015;
+                    break;
+            default: total = money * 1.05;
+                    break;
+        }
+        System.out.println(total);
+    }
+}
