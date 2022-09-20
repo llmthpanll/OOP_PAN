cd99ff59a0f39dfcb2a06b98dd0a3e2cd1081eea
Methasit Prateepmaethakul <54169574+llmthpanll@users.noreply.github.com>
2022-09-21 02:00:07 +0700

    s

--- week07/src/MnGUI.java
+++ week07/src/MnGUI.java
@@ -2,31 +2,34 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class MnGUI {
+public class MnGUI extends JFrame {
 
-    private JFrame fr;
-    private JMenuBar mb;
-    private JMenu m1;
-    private JMenu m2;
-    private JMenu m3;
-    private JMenuItem mi1;
-    private JMenuItem mi2;
-    private JMenuItem mi3;
-    private JMenuItem mi4;
+    private JDesktopPane desktopPane;
+    private JInternalFrame frame1, frame2;
 
-    public MnGui() {
-        fr = New JFrame;
-        
-        
-        
-        
-        
-        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fr.pack();
-        fr.setVisible(true);
+    public MnGUI() {
+        desktopPane = new JDesktopPane();
+        frame1 = new JInternalFrame("Frame 1", true, true, true, true);
+        frame2 = new JInternalFrame("Frame 2", true, true, true, true);
+        frame1.getContentPane().add(new JLabel("Frame 1 contents..."));
+        frame1.pack();
+        frame1.setVisible(true);
+        frame2.getContentPane().add(new JLabel("Frame 2 contents..."));
+        frame2.pack();
+        frame2.setVisible(true);
+        int x2 = frame1.getX() + frame1.getWidth() + 10;
+        int y2 = frame1.getY();
+        frame2.setLocation(x2, y2);
+        desktopPane.add(frame1);
+        desktopPane.add(frame2);
+        this.add(desktopPane, BorderLayout.CENTER);
+        this.setMinimumSize(new Dimension(300, 300));
+        this.pack();
+        this.setVisible(true);
+        this.setExtendedState(this.MAXIMIZED_BOTH);
     }
 
     public static void main(String[] args) {
-        new TellerGUI();
+        MnGUI frame = new MnGUI();
     }
 }
