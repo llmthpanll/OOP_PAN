498ea51519bc048be75b03ccadf91a28752bcdc9
Methasit Prateepmaethakul <54169574+llmthpanll@users.noreply.github.com>
2022-11-19 18:33:24 +0700

    .

---
+++ week13/src/MyTextEditorController.java
@@ -0,0 +1,11 @@
+public class MyTextEditorController {
+    private MyTextEditorView view;
+    private MyTextEditorModel model;
+
+    public MyTextEditorController(){
+        view = new MyTextEditorView();
+        model = new MyTextEditorModel();
+    }
+
+
+}
--- week13/src/MyTextEditorView.java
+++ week13/src/MyTextEditorView.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-public class MyTextEditor {
+public class MyTextEditorView {
     private JFrame fr;
     private JMenuBar mb;
     private JMenu m1;
     private JMenuItem mi1, mi2, mi3, mi4;
-    public MyTextEditor(){
+    public MyTextEditorView(){
         fr = new JFrame("My Text Editor");
         mb = new JMenuBar();
         m1 = new JMenu("File");
@@ -30,8 +30,60 @@ public class MyTextEditor {
         fr.setLocation(x, y);
         fr.setVisible(true);
     }
-    
-    public static void main(String[] args) {
-        new MyTextEditor();
+
+    public JFrame getFr() {
+        return fr;
+    }
+
+    public void setFr(JFrame fr) {
+        this.fr = fr;
+    }
+
+    public JMenuBar getMb() {
+        return mb;
+    }
+
+    public void setMb(JMenuBar mb) {
+        this.mb = mb;
+    }
+
+    public JMenu getM1() {
+        return m1;
+    }
+
+    public void setM1(JMenu m1) {
+        this.m1 = m1;
+    }
+
+    public JMenuItem getMi1() {
+        return mi1;
+    }
+
+    public void setMi1(JMenuItem mi1) {
+        this.mi1 = mi1;
+    }
+
+    public JMenuItem getMi2() {
+        return mi2;
+    }
+
+    public void setMi2(JMenuItem mi2) {
+        this.mi2 = mi2;
+    }
+
+    public JMenuItem getMi3() {
+        return mi3;
+    }
+
+    public void setMi3(JMenuItem mi3) {
+        this.mi3 = mi3;
+    }
+
+    public JMenuItem getMi4() {
+        return mi4;
+    }
+
+    public void setMi4(JMenuItem mi4) {
+        this.mi4 = mi4;
     }
 }
