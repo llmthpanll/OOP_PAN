bc12e1f2add590e7c489647701463cad6ac02135
Methasit Prateepmaethakul <54169574+llmthpanll@users.noreply.github.com>
2022-11-20 18:10:20 +0700

    week13 lab1

--- week13/src/MyTextEditorController.java
+++ week13/src/MyTextEditorController.java
@@ -1,11 +1,43 @@
-public class MyTextEditorController {
+
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+public class MyTextEditorController implements ActionListener {
+
     private MyTextEditorView view;
     private MyTextEditorModel model;
 
-    public MyTextEditorController(){
+    public MyTextEditorController() {
         view = new MyTextEditorView();
         model = new MyTextEditorModel();
+        init();
     }
 
+    public void init() {
+        view.getMi1().addActionListener(this);
+        view.getMi2().addActionListener(this);
+        view.getMi3().addActionListener(this);
+        view.getMi4().addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == view.getMi1()) {
+            view.getTextarea().setText("");
+        } else if (e.getSource() == view.getMi2()) {
+//            view.getFr().dispose();
+            JFileChooser fc = new JFileChooser();
+            fc.showOpenDialog(view.getFr()); // 
+            File f = fc.getSelectedFile();
+        } else if (e.getSource() == view.getMi3()) {
+//            view.getFr().dispose();
+            JFileChooser fc = new JFileChooser();
+            fc.showSaveDialog(view.getFr()); // fr คือ ตัวแปรออปเจ็ค JFrame
+            File f = fc.getSelectedFile();
+        } else if (e.getSource() == view.getMi4()) {
+            view.getFr().dispose();
+        }
+    }
 
 }
--- week13/src/MyTextEditorView.java
+++ week13/src/MyTextEditorView.java
@@ -1,11 +1,16 @@
+
 import javax.swing.*;
 import java.awt.*;
+
 public class MyTextEditorView {
+
     private JFrame fr;
     private JMenuBar mb;
     private JMenu m1;
     private JMenuItem mi1, mi2, mi3, mi4;
-    public MyTextEditorView(){
+    private JTextArea textarea;
+
+    public MyTextEditorView() {
         fr = new JFrame("My Text Editor");
         mb = new JMenuBar();
         m1 = new JMenu("File");
@@ -14,14 +19,16 @@ public class MyTextEditorView {
         mi2 = new JMenuItem("Open");
         mi3 = new JMenuItem("Save");
         mi4 = new JMenuItem("Close");
+        textarea = new JTextArea(20, 40);
+        fr.setLayout(new BorderLayout());
         m1.add(mi1);
         m1.add(mi2);
         m1.add(mi3);
         m1.addSeparator();
         m1.add(mi4);
         mb.add(m1);
-        fr.add(mb);
-        
+        fr.add(mb, BorderLayout.NORTH);
+        fr.add(textarea, BorderLayout.CENTER);
         fr.pack();
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
@@ -29,6 +36,14 @@ public class MyTextEditorView {
         int y = (int) ((dimension.getHeight() - fr.getHeight()) / 2);
         fr.setLocation(x, y);
         fr.setVisible(true);
+    }
+
+    public JTextArea getTextarea() {
+        return textarea;
+    }
+
+    public void setTextarea(JTextArea textarea) {
+        this.textarea = textarea;
     }
 
     public JFrame getFr() {
