f845118820fbfe2a200b371502bae3738813d9d7
EntityKunG <methasitpan@gmail.com>
2023-01-05 00:34:50 +0700

    .

---
+++ teach_nongnong/src/Main.java
@@ -0,0 +1,23 @@
+
+public class ClassShow {
+
+    public static void main(String[] args) {
+        System.out.println(
+        “Hello start
+        ”);
+ ClassShow m = new ClassShow();
+        m.show();
+        System.out.println(
+        “Hello end
+    
+
+    ”);
+} //end Main
+public void show() {
+        System.out.println(
+        “Hello world
+    
+
+”);
+} //end Show
+} //end class 
---
+++ test00/src/main/java/com/mycompany/test00/Test00.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test00;
+
+/**
+ *
+ * @author PAN
+ */
+public class Test00 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
